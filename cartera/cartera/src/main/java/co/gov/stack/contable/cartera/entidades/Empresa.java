891f8bf86a39bd3463513614d84da909aee108f9
fhordonez@unal.edu.co <fhordonezp@unal.edu.co>
2022-08-26 23:10:24 -0500

    Creacion atributos y metodos clase empresa

--- cartera/cartera/src/main/java/co/gov/stack/contable/cartera/entidades/Empresa.java
+++ cartera/cartera/src/main/java/co/gov/stack/contable/cartera/entidades/Empresa.java
@@ -1,4 +1,91 @@
 package co.gov.stack.contable.cartera.entidades;
 
 public class Empresa {
+
+    private long idEmpleado;
+    private String nombreEmpleado;
+    private String apellidoEmpleado;
+    private String correoEmpleado;
+    private String empresaEmpleado;
+    private String pass;
+    private String rolEmpleado;
+    private boolean estadoEmpleado;
+
+    public long getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(long idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getApellidoEmpleado() {
+        return apellidoEmpleado;
+    }
+
+    public void setApellidoEmpleado(String apellidoEmpleado) {
+        this.apellidoEmpleado = apellidoEmpleado;
+    }
+
+    public String getCorreoEmpleado() {
+        return correoEmpleado;
+    }
+
+    public void setCorreoEmpleado(String correoEmpleado) {
+        this.correoEmpleado = correoEmpleado;
+    }
+
+    public String getEmpresaEmpleado() {
+        return empresaEmpleado;
+    }
+
+    public void setEmpresaEmpleado(String empresaEmpleado) {
+        this.empresaEmpleado = empresaEmpleado;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
+
+    public boolean isEstadoEmpleado() {
+        return estadoEmpleado;
+    }
+
+    public void setEstadoEmpleado(boolean estadoEmpleado) {
+        this.estadoEmpleado = estadoEmpleado;
+    }
+
+    @Override
+    public String toString() {
+        return "Empresa{" +
+                "idEmpleado=" + idEmpleado +
+                ", nombreEmpleado='" + nombreEmpleado + '\'' +
+                ", apellidoEmpleado='" + apellidoEmpleado + '\'' +
+                ", correoEmpleado='" + correoEmpleado + '\'' +
+                ", empresaEmpleado='" + empresaEmpleado + '\'' +
+                ", pass='" + pass + '\'' +
+                ", rolEmpleado='" + rolEmpleado + '\'' +
+                ", estadoEmpleado=" + estadoEmpleado +
+                '}';
+    }
 }
