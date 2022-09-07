6f2aac0a961db3baff6768405210c6059e0881f5
fhordonez@unal.edu.co <fhordonezp@unal.edu.co>
2022-09-06 22:43:55 -0500

    metodos crud y creacion repositorio archivo cartera

--- cartera/cartera/src/main/java/co/gov/javastack/contable/cartera/service/MovimientoDineroService.java
+++ cartera/cartera/src/main/java/co/gov/javastack/contable/cartera/service/MovimientoDineroService.java
@@ -1,35 +1,42 @@
 package co.gov.javastack.contable.cartera.service;
 
 import co.gov.javastack.contable.cartera.entidades.MovimientoDinero;
+import co.gov.javastack.contable.cartera.repositorio.IMovimientoDineroRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MovimientoDineroService implements IMovimientoDineroService {
 
+    @Autowired
+    private IMovimientoDineroRepositorio movimientoDineroRepositorio;
     @Override
     public MovimientoDinero findById(int id) {
-        return null;
+        Optional<MovimientoDinero> movimientoDinero = movimientoDineroRepositorio.findById((long) id);
+        return movimientoDinero.get();
     }
 
     @Override
     public List<MovimientoDinero> findAll() {
-        return null;
+        List<MovimientoDinero> movimientoDinero = (List<MovimientoDinero>) movimientoDineroRepositorio.findAll();
+        return movimientoDinero;
     }
 
     @Override
     public MovimientoDinero createMovimientoDinero(MovimientoDinero movimientoDinero) {
-        return null;
+        return movimientoDineroRepositorio.save(movimientoDinero);
     }
 
     @Override
     public MovimientoDinero updateMovimientoDinero(MovimientoDinero movimientoDinero) {
-        return null;
+        return movimientoDineroRepositorio.save(movimientoDinero);
     }
 
     @Override
-    public void deteleMovimientoDinero(long id) {
+    public void deteleMovimientoDinero(long id) { movimientoDineroRepositorio.deleteById(id);
 
     }
 }
